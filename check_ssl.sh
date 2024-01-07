067fe4ad6c682f95046e9c5a43f369a2cd5812f7
zhk3r <37957791+zhk3r@users.noreply.github.com>
2024-01-07 11:44:33 +0100

    Update check_ssl.sh

--- check_ssl.sh
+++ check_ssl.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# ANSI color codes
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+RESET="\033[0m"
+
 # Connect to a hostname using openssl to show complete certificate chain.
 check-ssl() {
     openssl s_client --showcerts --connect "$1:443" 2>/dev/null | awk -v RED="$RED" -v GREEN="$GREEN" -v YELLOW="$YELLOW" -v BLUE="$BLUE" -v MAGENTA="$MAGENTA" -v CYAN="$CYAN" -v RESET="$RESET" '
