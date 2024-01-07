7e241655e16c347bebf84c3f053b2739ddc9d58d
zhk3r <37957791+zhk3r@users.noreply.github.com>
2024-01-07 16:09:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Update package lists
-sudo apt update -y
+sudo apt update
 
-# Install dependencies
-sudo apt install -y whois openssl curl lolcat
+# Install dependencies with automatic 'yes' to prompts
+sudo apt install -y whois openssl curl lolcat python3 python3-pip
+
+# Install Python packages with pip3
+pip3 install tabulate colorama
 
 # Define where to install the scripts
 install_dir="$HOME/check"
 
 # URLs of the raw scripts from the GitHub repository
 repo_base_url="https://raw.githubusercontent.com/zhk3r/check/testing"
-files=("check.sh" "check_cert.sh" "check_help.sh" "check_host.sh" "check_mail.sh" "check_rdns.sh" "check_ssl.sh" "check_update.sh" "hostguess.py" "install.sh" "rdns.py" "reverse-dns-lookup.py")
+files=("check.sh" "check_cert.sh" "check_help.sh" "check_host.sh" "check_mail.sh" "check_rdns.sh" "check_ssl.sh" "hostguess.py" "rdns.py" "reverse-dns-lookup.py")
 
 # Create the installation directory
 mkdir -p "$install_dir"
