d44f87f55b7f61bbc2a021d294820f1fb7a96ca8
zhk3r <37957791+zhk3r@users.noreply.github.com>
2024-01-11 09:47:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt update
 
 # Install dependencies with automatic 'yes' to prompts
-sudo apt install -y whois openssl dig curl lolcat grc boxes zsh python3 python3-pip
+sudo apt install -y git whois openssl dig curl lolcat grc boxes zsh python3 python3-pip
 
 # Install pip3 packages with sudo for system-wide installation
 sudo pip3 install tabulate
@@ -13,22 +13,11 @@ sudo pip3 install colorama
 # Define where to install the scripts
 install_dir="$HOME/check"
 
-# URLs of the raw scripts from the GitHub repository
-repo_base_url="https://raw.githubusercontent.com/zhk3r/check/testing"
-files=("check.sh" "check_cert.sh" "check_help.sh" "check_host.sh" "check_mail.sh" "check_rdns.sh" "check_ssl.sh" "check_norid.sh" "check_update.sh" "hostguess.py" "rdns.py" "reverse-dns-lookup.py")
+# Clone the entire repository
+repo_url="https://github.com/zhk3r/check.git"
+branch="testing"
+git clone --branch "$branch" "$repo_url" "$install_dir"
 
-# Create the installation directory
-mkdir -p "$install_dir"
-
-# Download and install each file
-for file in "${files[@]}"; do
-    echo "Downloading $file..."
-    curl -fsSL "$repo_base_url/$file" -o "$install_dir/$file"
-    chmod +x "$install_dir/$file"
-done
-
-echo "Installation complete. Please restart your shell or source your .bashrc/.zshrc file."
-echo "For ease of use, add an an alias for check.sh:"
-echo "Example: alias check='~/check/check.sh'"
-
-exec $SHELL
+# Make all downloaded scripts executable
+find "$install_dir" -type f -iname "*.sh" -exec chmod +x {} \;
+find "$install_dir" -type f -iname "*.py" -exec chmod +x {} \;
