cceb13b9effd027a2ca86bfa3edd5246f7ed8abc
zhk3r <37957791+zhk3r@users.noreply.github.com>
2024-01-11 22:09:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-# Update package lists
 sudo apt update
-
-# Install dependencies with automatic 'yes' to prompts
+# Dependencies
 sudo apt install -y git whois openssl dig curl grc boxes python3 python3-pip
-
 # Install pip3 packages with sudo for system-wide installation
 sudo pip3 install tabulate
 sudo pip3 install colorama
-
-# Define where to install the scripts
+# Install check testing branch
 install_dir="$HOME/check"
-
-# Clone the entire repository
 repo_url="https://github.com/zhk3r/check.git"
 branch="testing"
 git clone --branch "$branch" "$repo_url" "$install_dir"
-
 # Make all downloaded scripts executable
 find "$install_dir" -type f -iname "*.sh" -exec chmod +x {} \;
 find "$install_dir" -type f -iname "*.py" -exec chmod +x {} \;
