8eeb061625aca82ef9c1ac89af59dd26b30795ed
zhk3r <37957791+zhk3r@users.noreply.github.com>
2024-01-12 14:05:17 +0100

    Update check_update.sh

--- check_update.sh
+++ check_update.sh
@@ -3,19 +3,15 @@
 # Define where to install the scripts
 install_dir="$HOME/check"
 
-# URLs of the raw scripts from the GitHub repository
-repo_base_url="https://raw.githubusercontent.com/zhk3r/check/testing"
-files=("check.sh" "check_cert.sh" "check_help.sh" "check_host.sh" "check_mail.sh" "check_rdns.sh" "check_ssl.sh" "hostguess.py" "rdns.py" "reverse-dns-lookup.py")
+# URL of the GitHub repository branch
+repo_url="https://github.com/zhk3r/check"
+branch="testing"
 
-# Create the installation directory
-mkdir -p "$install_dir"
+# Clone the repository
+git clone --branch "$branch" "$repo_url" "$install_dir"
 
-# Download and install each file
-for file in "${files[@]}"; do
-    echo "Downloading $file..."
-    curl -fsSL "$repo_base_url/$file" -o "$install_dir/$file"
-    chmod +x "$install_dir/$file"
-done
+# Change file permissions to executable
+find "$install_dir" -type f -exec chmod +x {} \;
 
 echo "Installation complete. Please restart your shell or source your .bashrc/.zshrc file."
 echo
